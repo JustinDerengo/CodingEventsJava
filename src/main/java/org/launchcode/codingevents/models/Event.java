73fb300a6a91935a89d1bcd5c7e176ed31eb0f92
Allegorylla <138933736+JustinDerengo@users.noreply.github.com>
2023-09-27 07:19:04 -0600

    correct application.properties

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -5,11 +5,9 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import org.springframework.core.metrics.StartupStep;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Created by Chris Bay
